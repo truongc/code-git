a8aad998f45430d5499601029f9b198a20990aff
ctruong <tru.charles@gmail.com>
2014-05-02 10:57:58 +0200

    soumission du premier code
    ce code est l'application de l'article de Laurent sur la transformee de
    Fourier short term

---
+++ Exploration_STFT.m
@@ -0,0 +1,14 @@
+clearvars
+
+% contient les données
+cd('C:/Users/Charles/Documents/ENS Cachan/Donnees brutes/bon repère')
+
+% on lit un fichier
+filename = 'AKR-Zou-YO-1-Xsens-Pied Droit.csv'
+data = importdata(filename , ';')
+mat = data.data
+
+% on trace un signal
+% plot(
+
+x = colnames(data.data, 1:7 , data.colheaders)
